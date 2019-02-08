218c7034cf128b1e0736dadd001172138bce0d34
Bernat77 <bernatsalleraslopez@hotmail.com>
2019-02-08 03:40:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/berna/criminalintent/CrimeListActivity.java
@@ -0,0 +1,16 @@
+package com.example.berna.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
+
